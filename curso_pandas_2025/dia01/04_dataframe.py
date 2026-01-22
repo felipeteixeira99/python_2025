d9ce28ed6254ee94973d9557d4e71ec78afcbc4e
felipeteixeira99 <felipefggw@gmail.com>
2026-01-21 22:00:42 -0300

    data_frames e series

--- curso_pandas_2025/dia01/04_dataframe.py
+++ curso_pandas_2025/dia01/04_dataframe.py
@@ -4,7 +4,7 @@ import pandas as pd
 idades = [
     32,28,30,30,31,
     35,25,29,31,37,
-    27,23,36,33,33
+    27,23,36,33,35
 ]
 
 nomes = [
@@ -24,13 +24,17 @@ df["nomes"] = series_nomes
 df
 # %%
 df["nomes"]
+
+# %%
+df["idades"]
+# %%
+df["idades"]
+df["nomes"]
 # %%
 df.iloc[0]
 # %%
-
 df.iloc[0]["nomes"]
 
 # %%
-df.iloc[-1]['idades']
-
+df.iloc[-1]["nomes"]
 # %%
